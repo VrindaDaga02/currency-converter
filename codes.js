0e902628cec32a8b5f7e2a76fa81aef01cd64840
Vrinda Daga <112348809+VrindaDaga02@users.noreply.github.com>
2025-07-06 20:52:37 +0530

    Update codes.js

--- codes.js
+++ codes.js
@@ -1,12 +1,21 @@
-const currencyCodes = ["USD", "INR", "EUR", "GBP", "JPY", "AUD", "CAD"];
- 
+const currencyCodes = ["USD", "INR", "EUR", "GBP", "JPY", "AUD", "CAD", "CHF", "CNY", "SGD"];
+
 window.onload = () => {
   const from = document.getElementById("fromCurrency");
   const to = document.getElementById("toCurrency");
+
   currencyCodes.forEach(code => {
-    from.innerHTML += `<option value="${code}">${code}</option>`;
-    to.innerHTML += `<option value="${code}">${code}</option>`;
+    const option1 = document.createElement("option");
+    option1.value = code;
+    option1.text = code;
+    from.appendChild(option1);
+
+    const option2 = document.createElement("option");
+    option2.value = code;
+    option2.text = code;
+    to.appendChild(option2);
   });
+
   from.value = "USD";
   to.value = "INR";
 };
