2c01ccf00af902515a92ea178997aa95b253f44a
Vrinda Daga <112348809+VrindaDaga02@users.noreply.github.com>
2025-07-06 15:55:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 async function convertCurrency() {
   const amount = document.getElementById("amount").value;
-  const from = document.getElementById("fromCurrency").value;
-  const to = document.getElementById("toCurrency").value;
+  const from = document.getElementById("fromCurrency").value.toLowerCase();
+  const to = document.getElementById("toCurrency").value.toLowerCase();
 
   if (!amount || isNaN(amount)) {
     alert("Please enter a valid amount");
     return;
   }
 
-  const apiKey = "fca_live_MVpbkxpSOLlz8nsWlXDkX2YTiiBoAJS1GT4Jz6XV";
-  const url = `https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}&base_currency=${from}&currencies=${to}`;
+  const url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}/${to}.json`;
 
   try {
     const res = await fetch(url);
     const data = await res.json();
-    const rate = data.data[to];
+    const rate = data[to];
     const result = (amount * rate).toFixed(2);
-    document.getElementById("result").innerText = `${amount} ${from} = ${result} ${to}`;
+    document.getElementById("result").innerText = `${amount} ${from.toUpperCase()} = ${result} ${to.toUpperCase()}`;
   } catch (error) {
     document.getElementById("result").innerText = "Error fetching exchange rate.";
     console.error("API error:", error);
