b729a909859d29a62ba840b320febb1da200f695
Vrinda Daga <112348809+VrindaDaga02@users.noreply.github.com>
2025-07-06 21:00:18 +0530

    Update codes.js

--- codes.js
+++ codes.js
@@ -1,21 +1,55 @@
-const currencyCodes = ["USD", "INR", "EUR", "GBP", "JPY", "AUD", "CAD", "CHF", "CNY", "SGD"];
+const currencyMap = {
+  USD: "us",
+  INR: "in",
+  EUR: "eu",
+  GBP: "gb",
+  JPY: "jp",
+  AUD: "au",
+  CAD: "ca",
+  CHF: "ch",
+  CNY: "cn",
+  SGD: "sg"
+};
 
 window.onload = () => {
   const from = document.getElementById("fromCurrency");
   const to = document.getElementById("toCurrency");
 
-  currencyCodes.forEach(code => {
+  Object.keys(currencyMap).forEach(code => {
+    const countryCode = currencyMap[code];
+
     const option1 = document.createElement("option");
     option1.value = code;
-    option1.text = code;
+    option1.innerText = code;
+    option1.setAttribute("data-flag", countryCode);
     from.appendChild(option1);
 
     const option2 = document.createElement("option");
     option2.value = code;
-    option2.text = code;
+    option2.innerText = code;
+    option2.setAttribute("data-flag", countryCode);
     to.appendChild(option2);
   });
 
   from.value = "USD";
   to.value = "INR";
+
+  updateFlag(from);
+  updateFlag(to);
+
+  from.addEventListener("change", function () {
+    updateFlag(this);
+  });
+
+  to.addEventListener("change", function () {
+    updateFlag(this);
+  });
 };
+
+function updateFlag(selectElement) {
+  const selectedOption = selectElement.options[selectElement.selectedIndex];
+  const countryCode = selectedOption.getAttribute("data-flag");
+  if (countryCode) {
+    selectElement.style.backgroundImage = `url('https://flagcdn.com/24x18/${countryCode}.png')`;
+  }
+}
